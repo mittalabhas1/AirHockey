6d383ce87cfbdaf316eebd4cd6598b618c27f3e3
Abhas <mittalabhas1@gmail.com>
2014-05-30 04:18:57 +0530

    players moving

--- script.js
+++ script.js
@@ -22,8 +22,8 @@
 
 // }
 
-var c=document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
 
 
 var ball = {
@@ -34,23 +34,69 @@ var ball = {
     color: '#333'
 };
 
+var player = {
+	radius: 30
+}
+
+var player1 = {
+    x: 50,
+    y: 250,
+    radius: player.radius,
+    color: '#f00',
+    max_x: c.width/2 - player.radius,
+    min_x: player.radius,
+    max_y: c.height - player.radius,
+    min_y: player.radius
+};
+
+var player2 = {
+    x: 950,
+    y: 250,
+    radius: player.radius,
+    color: '#00f',
+    max_x: c.width - player.radius,
+    min_x: c.width/2 + player.radius,
+    max_y: c.height - player.radius,
+    min_y: player.radius
+};
+
 var bg = {
 	color: "white"
 };
 
+var players = [];
+players[0] = player1;
+players[1] = player2;
+
 render();
 
 function render() {
+
+	// making the center line
 	ctx.fillStyle = '#FFF';
     ctx.fillRect(0, 0, c.width, c.height);
     ctx.moveTo(500,0);
 	ctx.lineTo(500,500);
 	ctx.stroke();
+
+	//making the cue ball
 	ctx.beginPath();
 	ctx.arc(ball.x,ball.y,ball.radius,0,2*Math.PI);
 	ctx.fillStyle = ball.color;
 	ctx.fill();
 	ctx.strokeStyle = bg.color;
+	ctx.stroke();
+
+	//player1
+	ctx.beginPath();
+	ctx.arc(player1.x,player1.y,player1.radius,0,2*Math.PI);
+	ctx.strokeStyle = player1.color;
+	ctx.stroke();
+
+	//player2
+	ctx.beginPath();
+	ctx.arc(player2.x,player2.y,player2.radius,0,2*Math.PI);
+	ctx.strokeStyle = player2.color;
 	ctx.stroke();
 }
 
@@ -65,18 +111,16 @@ function coordinates(event){
 	moveBall(distance, slope, mousePos);
 }
 
-function getMousePos(c, evt) {
+function getMousePos(c, event) {
 	var rect = c.getBoundingClientRect();
 	return {
-	  x: evt.clientX - rect.left,
-	  y: evt.clientY - rect.top
+	  x: event.clientX - rect.left,
+	  y: event.clientY - rect.top
 	};
 }
-var time = Date.now();
-// setInterval(run, 10);
 
 function erase() {
-	console.log(ball.x+", "+ball.y+", "+distance);
+	// console.log(ball.x+", "+ball.y+", "+distance);
 	ctx.beginPath();
 	ctx.arc(ball.x,ball.y,ball.radius,0,2*Math.PI);
 	ctx.fillStyle = bg.color;
@@ -109,10 +153,13 @@ function moveBall(distance, slope, mousePos) {
 	}
 }
 
-// ctx.beginPath();
-// ctx.arc(50,250,30,0,2*Math.PI);
-// ctx.stroke();
-
-// ctx.beginPath();
-// ctx.arc(950,250,30,0,2*Math.PI);
-// ctx.stroke();
+function movePlayer(event, number) {
+	erase();
+	var mousePos = getMousePos(c, event);
+	// console.log(mousePos.x+", "+mousePos.y);
+	if(players[number-1].min_x <= mousePos.x && mousePos.x <= players[number-1].max_x && players[number-1].min_y <= mousePos.y && mousePos.y <= players[number-1].max_y){
+		players[number-1].x = mousePos.x;
+		players[number-1].y = mousePos.y;
+	}
+	render();
+}
